9fd5b6512cb1d180203a5aab9e9c05b2ce6a7e68
alokkumar954677-jpg <alokkumar954677@gmail.com>
2026-02-10 19:56:37 +0530

    Update set_method.py

--- set_method.py
+++ set_method.py
@@ -5,10 +5,10 @@ print(a,type(a))
 a.remove(4)# remove to word or digit
 print(a,type(a))
 print(len(a))
-a.pop()# rondam numer ko remov karta hai koi fix nahi hota hai
+a.pop()# rondam numer to remov  it can not  fix value
 print(a,type(a))
 b={1,6,7,8}
-print(a.union(b))# a or b ko jod kar ek list banata hai pr sem ko nhi likhe ga
-print(a.intersection(b))#a or b ke list se commn ko print karta hai 
-a.clear()# all list ko clear kar deta hai
+print(a.union(b))
+print(a.intersection(b))#a or b in list commn to print 
+a.clear()# all list to clear
 print(a)
